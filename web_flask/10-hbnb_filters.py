06278f4bad4724c9bef7ebd18f87adf0b68939ee
Abasilim Kosisochukwu Obaejeogu <obaejeoguabasilim@gmail.com>
2024-01-01 15:06:09 -0800

    deleted a file

---
+++ web_flask/10-hbnb_filters.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""Script that starts a Flask web application."""
+
+from flask import Flask
+from flask import render_template
+from models import storage
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+
+app = Flask(__name__)
+
+
+@app.route('/hbnb_filters', strict_slashes=False)
+def hbnb_filters():
+    """Display a HTML page with AirBnB filters."""
+    states = sorted(storage.all(State).values(), key=lambda state: state.name)
+    cities = sorted(storage.all(City).values(), key=lambda city: city.name)
+    amenities = sorted(storage.all(Amenity).values(), key=lambda amenity: amenity.name)
+    return render_template('10-hbnb_filters.html', states=states, cities=cities, amenities=amenities)
+
+
+@app.teardown_appcontext
+def teardown_db(exception):
+    """Remove the current SQLAlchemy Session."""
+    storage.close()
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
